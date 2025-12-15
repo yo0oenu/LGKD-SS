446d590400d8e95557c0153b48a99c4e87e46c74
yeonWoo <mu10adhd@gmail.com>
2025-12-16 04:19:11 +0900

    Update camvid_384x288_f2.py

--- configs/_base_/datasets/camvid_384x288_f2.py
+++ configs/_base_/datasets/camvid_384x288_f2.py
@@ -1,5 +1,5 @@
 dataset_type = 'camvidDataset'
-data_root = '/home/yeonwoo3/DATA/camvid/384x288_fold_p/fold2/'
+data_root = '//'
 # 데이터 전처리 파이프라인
 img_norm_cfg = dict(
     mean=[123.675, 116.28, 103.53], 
